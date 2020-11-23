1f2da5622224d17e3b050a0f62fd9ef18474d207
GoodStar20 <jameshart713@gmail.com>
2020-11-23 09:00:14 +0100

    change component name

---
+++ src/components/screens/Message.js
@@ -0,0 +1,48 @@
+import React, { useRef } from 'react';
+import { View, Text, ScrollView, KeyboardAvoidingView } from 'react-native';
+
+import Loading from 'src/components/screens/Loading';
+import styles from 'src/components/screens/styles/ChatListStyle';
+
+const Message = (props) => {
+  const { messages, customer, loading } = props;
+  const contentRef = useRef(null);
+
+  return (
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <KeyboardAvoidingView
+          behavior={Platform.OS == 'ios' ? 'padding' : null}
+          style={styles.messageContainer}
+        >
+          <ScrollView
+            style={styles.contentContainer}
+            scrollEnabled={true}
+            ref={contentRef}
+            onContentSizeChange={(contentWidth, contentHeight) => {
+              contentRef.current.scrollToEnd({ animated: true });
+            }}
+          >
+            <View style={{ flexGrow: 1 }}>
+              {messages?.length > 0 &&
+                messages.map((row, index) => (
+                  <View style={styles.messageRow} key={index}>
+                    {row.user === 'recipient' ? (
+                      <Text style={styles.recipient}>{customer}:</Text>
+                    ) : (
+                      <Text style={styles.sender}>You:</Text>
+                    )}
+                    <Text style={styles.message}>{row.message}</Text>
+                  </View>
+                ))}
+            </View>
+          </ScrollView>
+        </KeyboardAvoidingView>
+      )}
+    </>
+  );
+};
+
+export default Message;
